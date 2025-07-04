c450337a63a385ece2b9868e7f40de905a2334ec
calgreenex <carlos.alvarez@greenex.cl>
2025-07-04 12:17:38 -0400

    feat: Update logo and welcome page title
    This commit updates the application logo and the welcome page title.
    
    - Replaced the application logo image in `ApplicationLogo.jsx` from `/img/logo.png` to `/img/logonaombre.png`.
    - Updated the welcome page title in `Welcome.jsx` to "Bienvenido a Mi Legado Digital".
    - Added a background image to the `GuestLayout.jsx` and adjusted the z-index of the logo and content to ensure proper layering.

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -3,14 +3,18 @@ import { Link } from '@inertiajs/react';
 
 export default function GuestLayout({ children }) {
     return (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
-            <div>
+        <div className="relative flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
+            <div
+                className="absolute left-0 top-0 h-full w-1/2 bg-contain bg-no-repeat opacity-20"
+                style={{ backgroundImage: "url('/img/logo.png')" }}
+            ></div>
+            <div className="relative z-10">
                 <Link href="/">
                     <ApplicationLogo className="h-40 w-40 fill-current text-gray-500" />
                 </Link>
             </div>
 
-            <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
+            <div className="relative z-10 mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
                 {children}
             </div>
         </div>
